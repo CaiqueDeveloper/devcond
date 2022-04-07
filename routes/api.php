979e78c2ea3fac775e9e4c7834e8075acc217a11
Caique Bispo <caiquebispo0023@gmail.com>
2022-04-07 07:15:43 -0300

    Criando Estrutura base de rotas

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/ping', function(){
     return ['ping' => true];
 });
+
+Route::get('/401', [AuthController::class, 'aunathorized'])->name('login');
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+
+Route::middleware('auth:api')->group(function(){
+
+    Route::post('/auth/validate', [AuthController::class, 'validateToken']);
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+
+    //Mural de avisos
+    Route::get('/walls', [WallController::class, 'getAll']);
+    Route::post('/wall/{id}/like', [WallController::class, 'like']);
+
+    //Documentos
+    Route::get('/docs', [DocController::class, 'getAll']);
+
+    //Livro de Ocorrências
+    Route::get('/warnings', [WarningController::class, 'getMyWarning']);
+    Route::get('/warning', [WarningController::class, 'setWarning']);
+    Route::get('/warning/file', [WarningController::class, 'addWarningFile']);
+
+});
