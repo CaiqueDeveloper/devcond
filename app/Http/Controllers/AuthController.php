f14f92440c076629c4c02c0193eec1447b3ec4ce
Caique Bispo <caiquebispo0023@gmail.com>
2022-04-11 18:05:46 -0300

    Criando a rota de Register com action de login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Unit;
+use App\Models\User;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+     public function aunathorized(){
+        return response()->json(['Você não tem permissão para acesssar essa página.'], 401);
+     }
+
+    public function register(Request $request){
+
+        $array = ['error' => ''];
+        
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'cpf' => 'required|digits:11|unique:users,cpf',
+            'password' => 'required',
+            'password_comfirmed' => 'required|same:password',
+        ]);
+
+        if($validator->fails())
+        {
+            return $array['validatorErrors'] = $validator->errors()->first();
+        }
+
+        $newUser = new User();
+        $newUser->name = $request->input('name');
+        $newUser->email = $request->input('email');
+        $newUser->cpf = $request->input('cpf');
+        $newUser->password = password_hash($request->input('password'), PASSWORD_DEFAULT);
+        $newUser->save();
+        
+        $token = auth()->attempt([
+            'cpf' => $request->input('cpf'),
+            'password' => $request->input('password'),
+        ]);
+
+        if(!$token){
+           return $array['internalError'] = 'Ocorreu um erro interno.';
+        }
+
+        $array['user'] = auth()->user();
+        $array['user']['porperties'] = Unit::select('id', 'name')->where('id_woner', auth()->user()->id);
+        $array['token'] = $token;
+
+        return $array;
+    }
 }
